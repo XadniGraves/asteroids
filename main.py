3a4bc31bcec56266156114f416b6a2f9e08a386e
XadniGraves <xadnigraves@gmail.com>
2025-08-11 20:01:20 -0400

    added game loop, draws game window, quit button works, display updates

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import pygame
 from constants import *
 
+
+
 def main():
+
+    # Initialize Pygame
+    pygame.init()
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Set up the display
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Main game loop
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))  # Clear the screen with black
+
+    pygame.display.flip()  # Update the display
 
 if __name__ == "__main__":
     main()
